21bd3c84b7c53105b7460cd0f6c24466429fc737
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2024-10-08 21:41:54 +0530

    add

--- LINKED-LIST/Day5.cpp
+++ LINKED-LIST/Day5.cpp
@@ -1,138 +1,138 @@
 // Check if the given Linked List is Palindrome
 
-// #include <iostream>
-// #include <bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
-// class Node {
-// public:
+class Node {
+public:
     
-//     int data;   
+    int data;   
     
     
-//     Node* next;      
+    Node* next;      
 
     
-//     Node(int data1, Node* next1) {
-//         data = data1;
-//         next = next1;
-//     }
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
 
-//     Node(int data1) {
-//         data = data1;
-//         next = nullptr;
-//     }
-// };
-
-
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
 
 
 
-// // Broute force
-// bool isPalindrome(Node* head){
-//     Node* temp = head;
-//     stack<int> s;
-//     while(temp != nullptr){
-//         s.push(temp->data);
-//         temp = temp->next;
-//     }
-//     temp = head;
-//     while (temp != nullptr)
-//     {
-//         if(temp->data != s.top()){
-//             return false;
-//         }
 
-//         s.pop();
-//         temp = temp->next;
-//     }
+
+// Broute force
+bool isPalindrome(Node* head){
+    Node* temp = head;
+    stack<int> s;
+    while(temp != nullptr){
+        s.push(temp->data);
+        temp = temp->next;
+    }
+    temp = head;
+    while (temp != nullptr)
+    {
+        if(temp->data != s.top()){
+            return false;
+        }
+
+        s.pop();
+        temp = temp->next;
+    }
     
-// }
+}
 
   
 
 
 
 
-//Optical approach
-// Node* reverseLinkedList(Node *head)
-// {
+// Optical approach
+Node* reverseLinkedList(Node *head)
+{
    
-//    Node* temp = head;  
+   Node* temp = head;  
    
    
-//    Node* prev = NULL;  
+   Node* prev = NULL;  
    
 
-//    while(temp != NULL){  
-//        Node* front = temp->next;  
+   while(temp != NULL){  
+       Node* front = temp->next;  
        
-//        temp->next = prev;  
+       temp->next = prev;  
        
-//        prev = temp;  
+       prev = temp;  
       
-//        temp = front; 
-//    }
+       temp = front; 
+   }
   
-//    return prev;  
-// }
+   return prev;  
+}
 
 
-// Node*  funtomiddle(Node* head){
-//     Node* slow = head;
-//     Node* fast = head;
-//     while(fast->next != nullptr && fast->next->next != nullptr){
-//         fast = fast->next->next;
-//         slow = slow->next;
-//     }
+Node*  funtomiddle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast->next != nullptr && fast->next->next != nullptr){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
 
-//     return slow;
-// }
-
-
-// bool isPalindrom(Node* head){
-//      if(head == nullptr || head->next == nullptr){
-//         return true;
-//     }
-//     Node* temp = head;
-
-//     Node* middle = funtomiddle(head);
-//     Node* middle1 = reverseLinkedList(middle->next); 
-//     while(middle1 != nullptr){
-//         if(temp->data != middle1->data){
-//             return false;
-//         }
-
-//         temp = temp->next;
-//         middle1 = middle1->next;
-//     }
-//     return true;
-// }
+    return slow;
+}
 
 
+bool isPalindrom(Node* head){
+     if(head == nullptr || head->next == nullptr){
+        return true;
+    }
+    Node* temp = head;
 
+    Node* middle = funtomiddle(head);
+    Node* middle1 = reverseLinkedList(middle->next); 
+    while(middle1 != nullptr){
+        if(temp->data != middle1->data){
+            return false;
+        }
+
+        temp = temp->next;
+        middle1 = middle1->next;
+    }
+    return true;
+}
 
 
 
-// int main() {
+
+
+
+int main() {
     
-//     Node* head = new Node(1);
-//     head->next = new Node(2);
-//     // head->next->next = new Node(2);
-//     // head->next->next->next = new Node(5);
-//     // head->next->next->next->next = new Node(1);
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(5);
+    head->next->next->next->next = new Node(1);
 
 
 
-//     if (isPalindrom(head)) {
-//         cout << "The linked list is a palindrome." << endl;
-//     } else {
-//         cout << "The linked list is not a palindrome." << endl;
-//     }
+    if (isPalindrom(head)) {
+        cout << "The linked list is a palindrome." << endl;
+    } else {
+        cout << "The linked list is not a palindrome." << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -246,5 +246,90 @@ int main()
     ListNode *newHead = SegregatetoOddEVen(head);
     cout << "LinkedList After Segregation: " << endl;
     PrintList(newHead);
+    return 0;
+}
+
+
+
+// 	Remove Nth node from the back of the LL
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+void printLL(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+// Broute force
+Node* removeNthFromEnd(Node* head, int n) {
+        if(head == nullptr){
+            return head;
+        }
+        Node* temp = head;
+        int cnt = 0;
+        while(temp != nullptr){
+            cnt++;
+            temp = temp->next;
+        }
+        if(cnt == n){
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return head; 
+        }
+        int pos = cnt - n;
+        int cnt1 = 0;
+        temp = head;
+        while(temp != nullptr){
+            if (cnt1 == pos - 1) {
+                Node* nodeToRemove = temp->next;
+                temp->next = nodeToRemove->next;
+                delete nodeToRemove;
+                break;
+            }
+            temp = temp->next;
+            cnt1++;
+        }
+        return head;
+}
+
+
+
+
+// optimal approach
+Node* removeNthFromEnd(Node* head, int n){
+    
+}
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int N = 3;
+    Node* head = new Node(arr[0]);
+    head->next = new Node(arr[1]);
+    head->next->next = new Node(arr[2]);
+    head->next->next->next = new Node(arr[3]);
+    head->next->next->next->next = new Node(arr[4]);
+
+    head = removeNthFromEnd(head, N);
+    printLL(head);
     return 0;
 }
