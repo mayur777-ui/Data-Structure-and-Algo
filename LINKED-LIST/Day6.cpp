0759d30c4c175a625db3d3d9e3ab6d6b40a6e0a2
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-10-10 01:25:58 +0530

    Update Day6.cpp

--- LINKED-LIST/Day6.cpp
+++ LINKED-LIST/Day6.cpp
@@ -1,112 +1,113 @@
-// // Delete the Middle Node of the Linked List
+// Delete the Middle Node of the Linked List
 
 
-// #include <iostream>
-// #include <bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
-// class Node {
-// public:
-//     int data;
-//     Node* next;
+class Node {
+public:
+    int data;
+    Node* next;
 
-//     Node(int data1, Node* next1) {
-//         data = data1;
-//         next = next1;
-//     }
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
 
-//     Node(int data1) {
-//         data = data1;
-//         next = nullptr;
-//     }
-// };
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
 
 
 
-// // Broutel force
-// Node* deleteMiddle(Node* head) {
-//     Node* temp = head;
-//     int n = 0;
+// Broutel force
+Node* deleteMiddle(Node* head) {
+    Node* temp = head;
+    int n = 0;
     
-//     while(temp != nullptr) {
-//         n++;
-//         temp = temp->next;
-//     }
+    while(temp != nullptr) {
+        n++;
+        temp = temp->next;
+    }
 
-//     int res = n / 2;
-//     temp = head;
+    int res = n / 2;
+    temp = head;
 
-//     while(temp != nullptr) {
-//         res--;
-//         if (res == 0) {
-//             Node* middle = temp->next;
-//             temp->next = temp->next->next;
-//             free(middle);
-//             break;
-//         }
-//         temp = temp->next;
-//     }
+    while(temp != nullptr) {
+        res--;
+        if (res == 0) {
+            Node* middle = temp->next;
+            temp->next = temp->next->next;
+            free(middle);
+            break;
+        }
+        temp = temp->next;
+    }
 
-//     return head;
-// }
-
-
+    return head;
+}
 
 
-// // Optimal Approach
-// Node* deleteMiddle(Node* head) {
-//     if (!head || !head->next) {
-//         delete head;
-//         return nullptr;
-//     }
-
-//     Node* slow = head;
-//     Node* fast = head->next->next;
-
-//     while (fast != nullptr && fast->next != nullptr) {
-//         slow = slow->next;
-//         fast = fast->next->next;
-//     }
-
-//     Node* temp = slow->next;
-//     slow->next = slow->next->next;
-//     delete temp;
-
-//     return head;
-// }
 
 
-// void printLL(Node* head) {
-//     Node* temp = head;
-//     while (temp != nullptr) {
-//         cout << temp->data << " ";
-//         temp = temp->next;
-//     }
-//     cout << endl;
-// }
+// Optimal Approach
+Node* deleteMiddle(Node* head) {
+    if (!head || !head->next) {
+        delete head;
+        return nullptr;
+    }
 
-// int main() {
-//     Node* head = new Node(1);
-//     head->next = new Node(2);
-//     head->next->next = new Node(3);
-//     head->next->next->next = new Node(4);
-//     head->next->next->next->next = new Node(5);
+    Node* slow = head;
+    Node* fast = head->next->next;
 
-//     cout << "Original Linked List: ";
-//     printLL(head);
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
 
-//     head = deleteMiddle(head);
+    Node* temp = slow->next;
+    slow->next = slow->next->next;
+    delete temp;
 
-//     cout << "Updated Linked List: ";
-//     printLL(head);
+    return head;
+}
 
-//     return 0;
-// }
+
+void printLL(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original Linked List: ";
+    printLL(head);
+
+    head = deleteMiddle(head);
+
+    cout << "Updated Linked List: ";
+    printLL(head);
+
+    return 0;
+}
 
 
 
 // Sort a Linked List
+
 #include <iostream>
 #include <bits/stdc++.h> 
 
@@ -190,42 +191,22 @@ void printLinkedList(Node* head){
     cout << endl;
 }
 
-<<<<<<< HEAD
-=======
 int main() {
-    Node* head = new Node(1);
+    // Linked List: 3 2 5 4 1
+    Node* head = new Node(3);
     head->next = new Node(2);
-    head->next->next = new Node(3);
+    head->next->next = new Node(5);
     head->next->next->next = new Node(4);
-    head->next->next->next->next = new Node(5);
+    head->next->next->next->next = new Node(1);
 
     cout << "Original Linked List: ";
-    printLL(head);
+    printLinkedList(head);
 
-    head = deleteMiddle(head);
+    // Sort the linked list
+    head =  Sll(head);
 
-    cout << "Updated Linked List: ";
-    printLL(head);
+    cout << "Sorted Linked List: ";
+    printLinkedList(head);
 
+    return 0;
 }
->>>>>>> fef880231d620cc758336f68df29eaa8c9bec79c
-
-// int main() {
-//     // Linked List: 3 2 5 4 1
-//     Node* head = new Node(3);
-//     head->next = new Node(2);
-//     head->next->next = new Node(5);
-//     head->next->next->next = new Node(4);
-//     head->next->next->next->next = new Node(1);
-
-//     cout << "Original Linked List: ";
-//     printLinkedList(head);
-
-//     // Sort the linked list
-//     head =  Sll(head);
-
-//     cout << "Sorted Linked List: ";
-//     printLinkedList(head);
-
-//     return 0;
-// }
