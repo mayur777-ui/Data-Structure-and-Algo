40d6fd77e5db0207cec8056aefe87863d0d270f2
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2024-10-11 17:02:55 +0530

    add

--- LINKED-LIST/Day7.cpp
+++ LINKED-LIST/Day7.cpp
@@ -114,3 +114,102 @@ int main() {
     
     return 0;
 }
+
+
+
+// Add two numbers represented as Linked Lists
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
+        }
+        return dummy -> next; 
+    }
+};
+
+void printList(ListNode* node) {
+    while (node != nullptr) {
+        std::cout << node->val;
+        if (node->next != nullptr) {
+            std::cout << " -> ";
+        }
+        node = node->next;
+    }
+    std::cout << std::endl;
+}
+
+
+ListNode* createList(const std::vector<int>& values) {
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    for (int value : values) {
+        ListNode* newNode = new ListNode(value);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    return head;
+}
+
+int main() {
+    Solution solution;
+
+
+    ListNode* l1 = createList({2, 4, 3});
+    ListNode* l2 = createList({5, 6, 4});
+
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+    std::cout << "Result: ";
+    printList(result);
+
+   
+    while (l1 != nullptr) {
+        ListNode* temp = l1;
+        l1 = l1->next;
+        delete temp;
+    }
+    while (l2 != nullptr) {
+        ListNode* temp = l2;
+        l2 = l2->next;
+        delete temp;
+    }
+    while (result != nullptr) {
+        ListNode* temp = result;
+        result = result->next;
+        delete temp;
+    }
+
+    return 0;
+}
