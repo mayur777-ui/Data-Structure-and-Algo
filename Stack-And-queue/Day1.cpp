0f7c75e887cc83c36947eef123538847b3a56354
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2024-10-13 19:33:16 +0530

    add

--- Stack-And-queue/Day1.cpp
+++ Stack-And-queue/Day1.cpp
@@ -307,4 +307,55 @@ int main() {
 
 
 
-// helllow osngoibgiubsdfiiudibnfiber
+// IMPLEMENTATION OF STACK USING IN-BUILT QUEUE;
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    queue <int> q;
+    public:
+    void Push(int data){
+        int s = q.size();
+        q.push(data);
+        for(int i = 0; i < s; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        if (q.empty()) {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        int temp = q.front();
+        q.pop();
+        return temp;
+    }
+    int top() {
+        if (q.empty()) {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        return q.front();
+    }
+     bool isEmpty() {
+        return q.empty();
+    }
+};
+
+
+int main() {
+    Stack s;
+    s.Push(10);
+    s.Push(20);
+    s.Push(30);
+
+    cout << "Top element: " << s.top() << endl;  
+    cout << "Popped element: " << s.pop() << endl; 
+    cout << "Popped element: " << s.pop() << endl; 
+    cout << "Top element: " << s.top() << endl;
+
+    return 0;
+}
