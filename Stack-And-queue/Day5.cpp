3f468d4901bce95074985320518cfd8d0f299097
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-10-17 13:27:14 +0530

    Update Day5.cpp

--- Stack-And-queue/Day5.cpp
+++ Stack-And-queue/Day5.cpp
@@ -50,6 +50,35 @@ int trap(vector<int> arr){
 }
 
 
+
+
+// Optimal approah;
+int trap(vector<int>& arr) {
+    int n = arr.size();
+    int left = 0, right = n - 1;
+    int maxleft = 0, maxRight = 0;
+    int res = 0;
+    while(left <= right){
+        if(arr[left] <= arr[right]){
+            if(arr[left] >= maxleft){
+                maxleft = arr[left];
+            }else{
+                res += maxleft - arr[left];
+            }
+            left++;
+        }else{
+            if(arr[right] >= maxRight){
+                maxRight = arr[right];
+            }else{
+                res += maxRight - arr[right];
+            }
+            right--;
+        }
+    }
+    return res;
+}
+
+
 int main() {
   vector < int > arr;
   arr = {0,1,0,2,1,0,1,3,2,1,2,1};
