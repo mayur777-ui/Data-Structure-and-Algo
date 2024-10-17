2b5f6fb60b9a3240f4e05a4c3862abacd9df5492
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-10-17 13:26:59 +0530

    Update Day5.cpp

--- Stack-And-queue/Day5.cpp
+++ Stack-And-queue/Day5.cpp
@@ -4,29 +4,50 @@ using namespace std;
 
 // broute force
 
-// int trap(vector<int> arr){
-//     int n = arr.size();
-//     int ans = 0;
-//     for(int i = 0; i < n; i++){
-//         int j  = i;
-//         int leftmax = 0;
-//         int rightmax = 0;
-//         while(j >= 0){
-//             leftmax = max(leftmax,arr[j]);
-//             j--;
-//         }
-//         j = i;
-//         while(j < n){
-//             rightmax = max(rightmax, arr[j]);
-//             j++;
-//         }
-//         ans += min(leftmax,rightmax) - arr[i];
-//     }
-//     return ans;
-// }
+int trap(vector<int> arr){
+    int n = arr.size();
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int j  = i;
+        int leftmax = 0;
+        int rightmax = 0;
+        while(j >= 0){
+            leftmax = max(leftmax,arr[j]);
+            j--;
+        }
+        j = i;
+        while(j < n){
+            rightmax = max(rightmax, arr[j]);
+            j++;
+        }
+        ans += min(leftmax,rightmax) - arr[i];
+    }
+    return ans;
+}
 
 
 
+
+// better solution
+int trap(vector<int> arr){
+    int n = arr.size();
+    vector<int> prefix(n);
+    vector<int> suffix(n);
+    prefix[0] = arr[0];
+    suffix[n - 1] = arr[n - 1];
+    for(int  i = 1; i < n; i++){
+        prefix[i] = max(prefix[i - 1],arr[i]);
+    }
+    for(int i = n - 2; i >= 0 ;i--){
+        suffix[i] = max(suffix[i + 1], arr[i]);
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += min(prefix[i],suffix[i]) - arr[i];
+    }
+    return ans;
+}
 
 
 int main() {
