c6bb167a928648a4a09f07763ae1a702b0ed3dfb
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2024-11-04 22:11:08 +0530

    add

--- Tree/Day2.cpp
+++ Tree/Day2.cpp
@@ -14,19 +14,6 @@ public:
 
 
 
-TreeNode* sortedArrayToBST(vector<int>& arr, int start, int end) {
-     if (start > end) {
-        return nullptr;
-    }
-    int mid = start + (end - start) / 2;
-    TreeNode* root = new TreeNode(arr[mid]);
-    root->left = sortedArrayToBST(arr, start, mid - 1);
-    root->right = sortedArrayToBST(arr, mid + 1, end);
-
-    return root;
-}
-
-
 
 
 void inOrderTraversal(TreeNode* root) {
