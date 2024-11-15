13f403dfecc78654839dc68a28b9177f5d0c49b9
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-11-15 11:32:16 +0530

    Update Day1.cpp
    add

--- Heap/Day1.cpp
+++ Heap/Day1.cpp
@@ -76,3 +76,51 @@ int main(){
     heap.deleteion();
     heap.print();
 }
+
+
+// By an given arr making an  heap
+void heapify(int arr[], int i, int n){
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if(left < n && arr[left] > arr[largest]){
+        largest = left;
+    }
+    if(right < n && arr[right] > arr[largest]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[i],arr[largest]);
+        heapify(arr, largest, n);
+    }
+}
+void builtheap(int arr[],int n){
+    for(int i = n/2 - 1; i >= 0; i--){
+        heapify(arr,i,n);
+    }
+}
+
+// heap sort
+
+void heapsort(int arr[],int n){
+    builtheap(arr,n);
+    for(int i = n - 1; i >= 0; i--){
+        swap(arr[0],arr[i]);
+        heapify(arr,0,i);
+    }
+}
+
+void print(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    int arr[] = {10,3 ,8 ,9 ,5, 13 ,18 , 14 , 11 ,70};
+    // builtheap(arr,10);
+    heapsort(arr,10);
+    print(arr,10);
+}
+
