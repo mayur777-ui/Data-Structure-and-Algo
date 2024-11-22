ac09e97a1673a2f2b5ad16cd160f2c0e5f11eb83
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-11-22 21:33:53 +0530

    Update Day4.cpp

--- Tree/Day4.cpp
+++ Tree/Day4.cpp
@@ -1,1 +1,79 @@
-hello
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+
+
+// floor 
+int findFloor(Node* root, int key) {
+    int floor = -1;
+    while (root) {
+        if (root->data == key) {
+            return root->data; // Exact match
+        }
+        if (root->data < key) {
+            floor = root->data; // Update floor
+            root = root->right;
+        } else {
+            root = root->left;
+        }
+    }
+    return floor;
+}
+
+
+int findCeil(Node* root, int key) {
+    int floor = -1;
+    while (root) {
+        if (root->data == key) {
+            return root->data; // Exact match
+        }
+        if (root->data > key) {
+            floor = root->data; // Update floor
+            root = root->left;
+        } else {
+            root = root->right;
+        }
+    }
+    return floor;
+}
+
+
+
+Node* insert(Node* root, int key) {
+    if (!root) return new Node(key);
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else
+        root->right = insert(root->right, key);
+    return root;
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 8);
+    root = insert(root, 4);
+    root = insert(root, 12);
+    root = insert(root, 2);
+    root = insert(root, 6);
+    root = insert(root, 10);
+    root = insert(root, 14);
+
+    int key = 5;
+
+    cout << "Floor of " << key << " is " << findFloor(root, key) << endl;
+    cout << "Ceil of " << key << " is " << findCeil(root, key) << endl;
+
+    return 0;
+}
