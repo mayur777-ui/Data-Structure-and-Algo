9efc816d293d3ea06771c1a21484041161b22503
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-02-13 16:27:44 +0530

    restart dsa with two pointer

---
+++ Two-pointer-approach/Day1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    if (s.length() == 0) {
+        return 0;
+    }
+    int len = 0;
+    for (int i = 0; i < s.length(); i++) {
+        unordered_set<char> set; 
+        for (int j = i; j < s.length(); j++) {
+            if (set.find(s[j]) != set.end()) { 
+                len = max(len, j - i);
+                break; 
+            }
+            set.insert(s[j]); 
+        }
+        len = max(len, (int)set.size());
+    }
+    return len;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << "The length of the longest substring without repeating characters is " 
+         << lengthOfLongestSubstring(s) << endl;
+    return 0;
+}
