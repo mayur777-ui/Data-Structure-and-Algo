4dcd30a1486f836bd3374b2dc2e5f6cd16e53f56
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-03-08 18:29:11 +0530

    adding one more problem name as search 2d Matrix

--- ARRAY/Day8.cpp
+++ ARRAY/Day8.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // 	Find the row with maximum number of 1's
 
-// Broute force
+// Brute force
 int rowWithMax1s(vector<vector<int>>mat, int n,int m){
     int max_ones = 0;
     int index = -1;
@@ -39,8 +39,6 @@ int lower_bound(vector<int> mat, int m, int key){
         if(mat[mid] == key){
             ans = min(ans,mid);
             high = mid - 1;
-        }else if(mat[mid] > key){
-            high = mid - 1;
         }else{
             low = mid + 1;
         }
@@ -70,3 +68,84 @@ int main()
     cout << "The row with maximum no. of 1's is: " <<
          rowWithMax1s(matrix, n, m) << '\n';
 }
+
+
+
+
+
+
+// Search in a sorted 2D matrix
+
+
+// Brute force
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size(), 
+    m =(n > 0)?matrix[0].size():0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+// Optimal approach 1.
+
+bool searchInrow(vector<int> mat, int m,int key){
+    int low = 0;
+    int high = m - 1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(mat[mid] == key){
+            return true;
+        }else if(mat[mid] < key){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return false;
+}
+
+
+// Optimal approach 2.
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int low = 0;
+    int high = n * m - 1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        int row = mid/m;
+        int col = mid%m;
+        if(matrix[row][col] == target){
+            return true;
+        }else if(matrix[row][col] < target) {
+            low = mid+1;
+        }
+        else {
+            high = mid -1;
+        }
+    }
+    return false;
+}
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n =  matrix.size();
+    int m =  (n > 0)?matrix[0].size() : 0; 
+    for(int i = 0; i < n;i++){
+        if(searchInrow(matrix[i],m,target)){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> mat = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    cout << searchMatrix(mat, 3);
+}
+
+
