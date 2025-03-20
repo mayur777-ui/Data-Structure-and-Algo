aa9e814f6bb090db26b961c5fe0820c4cbc4eec3
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2025-03-20 17:27:48 +0530

    Update Day4.cpp
    add roman to int

--- String/Day4.cpp
+++ String/Day4.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 
 
-// // RomanTOInt 
-// int romanToInt(string s) {
-//     map<char, int> mp = {
-//         {'I', 1}, {'V', 5}, {'X', 10},
-//         {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-//     };
+// RomanTOInt 
+int romanToInt(string s) {
+    map<char, int> mp = {
+        {'I', 1}, {'V', 5}, {'X', 10},
+        {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+    };
 
-//     int sum = 0;
+    int sum = 0;
 
-//     for (int i = 0; i < s.size(); i++) {
-//         if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) {
-//             sum -= mp[s[i]];  
-//         } else {
-//             sum += mp[s[i]];  
-//         }
-//     }
+    for (int i = 0; i < s.size(); i++) {
+        if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) {
+            sum -= mp[s[i]];  
+        } else {
+            sum += mp[s[i]];  
+        }
+    }
 
-//     return sum;
-// }
+    return sum;
+}
 
 
 int main(){
