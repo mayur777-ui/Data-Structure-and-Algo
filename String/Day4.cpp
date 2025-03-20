6240d1ee2cf64fdd8122d9252d8fd5d93f9c240d
{mayur777-ui} <{mayurlaxkar76@gamil.com}>
2025-03-20 18:36:22 +0530

    add

--- String/Day4.cpp
+++ String/Day4.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// // RomanTOInt
-// int romanToInt(string s) {
-//     map<char, int> mp = {
-//         {'I', 1}, {'V', 5}, {'X', 10},
-//         {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-//     };
+// RomanTOInt
+int romanToInt(string s) {
+    map<char, int> mp = {
+        {'I', 1}, {'V', 5}, {'X', 10},
+        {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+    };
 
-//     int sum = 0;
+    int sum = 0;
 
-//     for (int i = 0; i < s.size(); i++) {
-//         if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) {
-//             sum -= mp[s[i]];
-//         } else {
-//             sum += mp[s[i]];
-//         }
-//     }
+    for (int i = 0; i < s.size(); i++) {
+        if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) {
+            sum -= mp[s[i]];
+        } else {
+            sum += mp[s[i]];
+        }
+    }
 
-//     return sum;
-// }
+    return sum;
+}
 
-// int main(){
-//     string s = "MCMXCIV";
-//     int res = romanToInt(s);
-//     cout << res;
-// }
+int main(){
+    string s = "MCMXCIV";
+    int res = romanToInt(s);
+    cout << res;
+}
 
 string InttoRoman(int num) {
     map<int, string, greater<int>> mp = {
