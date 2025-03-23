155444ecf46b3a7923f9e2e7d6d378df0acd6597
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2025-03-23 11:36:18 +0530

    Update Day6.cpp
    add all

--- Tree/Day6.cpp
+++ Tree/Day6.cpp
@@ -27,82 +27,82 @@ TreeNode* convertotree(vector<int> arr, int i){
 
 
 // Approach 1.(iteretive approach)
-// void Path_of_root_to_each_leaf(TreeNode* root){
-//     if(root == nullptr){
-//         cout << "not any leaf found";
-//         return;
-//     }
-//     stack<pair<TreeNode*,vector<int>>> st;
-//     st.push({root,{root->val}});
-//     while(!st.empty()){
-//         TreeNode* node = st.top().first;
-//         vector<int> path = st.top().second;
-//         st.pop();
-//         if(!node->left && !node->right){
-//             for(int i = 0; i < path.size(); i++){
-//                 cout << path[i] << " ";
-//             }
-//             cout << endl;
-//         }
+void Path_of_root_to_each_leaf(TreeNode* root){
+    if(root == nullptr){
+        cout << "not any leaf found";
+        return;
+    }
+    stack<pair<TreeNode*,vector<int>>> st;
+    st.push({root,{root->val}});
+    while(!st.empty()){
+        TreeNode* node = st.top().first;
+        vector<int> path = st.top().second;
+        st.pop();
+        if(!node->left && !node->right){
+            for(int i = 0; i < path.size(); i++){
+                cout << path[i] << " ";
+            }
+            cout << endl;
+        }
 
-//         if(node->right){
-//             vector<int> newPath = path;
-//             newPath.push_back(node->right->val);
-//             st.push({node->right,newPath});
-//         }
+        if(node->right){
+            vector<int> newPath = path;
+            newPath.push_back(node->right->val);
+            st.push({node->right,newPath});
+        }
 
-//         if(node->left){
-//             vector<int> newPath = path;
-//             newPath.push_back(node->left->val);
-//             st.push({node->left,move(newPath)});
-//         }
-//     }
+        if(node->left){
+            vector<int> newPath = path;
+            newPath.push_back(node->left->val);
+            st.push({node->left,move(newPath)});
+        }
+    }
 
-// }
-
-
+}
 
 
-// // Approach 2.(recursive approach)
-// void Path_of_root_to_each_leaf(TreeNode* root, vector<int>&path){
-//     if(root == nullptr){
-//         return;
-//     }
 
-//     path.push_back(root->val);
+
+// Approach 2.(recursive approach)
+void Path_of_root_to_each_leaf(TreeNode* root, vector<int>&path){
+    if(root == nullptr){
+        return;
+    }
+
+    path.push_back(root->val);
     
-//     if(!root->left && !root->right){
-//         for(int i = 0; i < path.size(); i++){
-//             cout << path[i]  << " ";
-//         }
-//         cout << endl;
-//     }
+    if(!root->left && !root->right){
+        for(int i = 0; i < path.size(); i++){
+            cout << path[i]  << " ";
+        }
+        cout << endl;
+    }
 
-//     Path_of_root_to_each_leaf(root->left, path);
-//     Path_of_root_to_each_leaf(root->right,path);
+    Path_of_root_to_each_leaf(root->left, path);
+    Path_of_root_to_each_leaf(root->right,path);
 
-//     path.pop_back();
-// }
+    path.pop_back();
+}
 
 
-// int dfs(TreeNode* root,int&maxSum){
-//     if(!root){
-//         return 0;
-//     }
+int dfs(TreeNode* root,int&maxSum){
+    if(!root){
+        return 0;
+    }
 
-//     int lmax = max(dfs(root->left,maxSum),0);
-//     int rmax = max(dfs(root->right,maxSum),0);
+    int lmax = max(dfs(root->left,maxSum),0);
+    int rmax = max(dfs(root->right,maxSum),0);
 
-//     int currSum = root->val + lmax + rmax;
-//     maxSum = max(maxSum,currSum);
-//     return root->val + max(lmax,rmax);
-// }
+    int currSum = root->val + lmax + rmax;
+    maxSum = max(maxSum,currSum);
+    return root->val + max(lmax,rmax);
+}
 
-// int maximumPathSum(TreeNode* root){
-//     int maxSum = INT_MIN;
-//     dfs(root,maxSum);
-//     return maxSum;
-// }
+int maximumPathSum(TreeNode* root){
+    int maxSum = INT_MIN;
+    dfs(root,maxSum);
+    return maxSum;
+}
 
 
 int main(){
