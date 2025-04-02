78e429fdf0d47347c38896ae620fc12d7b02e957
mayur777-ui <mayurlaxkar76@gmail.com>
2025-04-02 16:16:14 +0530

    add dfs method of surrounding region

--- Graphs/Day5.cpp
+++ Graphs/Day5.cpp
@@ -46,9 +46,9 @@ int main(){
     }
 }
 
-// Surrounded Regions (Leetcode-130) - BFS Solution
-#include <bits/stdc++.h>
-using namespace std;
+// Surrounded Regions (Leetcode-130) -
+// 1. BFS Solution
+
 
 void solve(vector<vector<char>>&board) {
     int n = board.size();
@@ -94,6 +94,38 @@ void solve(vector<vector<char>>&board) {
     }
 }
 
+
+// 2.Dfs
+
+void dfs(vector<vector<char>>&board,int&n,int&m,int i, int j){
+    if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O'){
+        return;
+    }
+
+    board[i][j] = '#';
+    dfs(board,n,m,i - 1,j);
+    dfs(board,n,m,i + 1, j);
+    dfs(board,n,m,i,j - 1);
+    dfs(board,n,m, i, j + 1);
+}
+void solve(vector<vector<char>>&board){
+    int n = board.size();
+    int m = board[0].size();
+    for(int i = 0; i < n; i++){
+        if(board[i][0] == 'O') dfs(board,n,m,i,0);
+        if(board[i][m - 1] == 'O') dfs(board,n,m,i,n - 1);
+    }
+    for(int j = 0; j < m; j++){
+        if(board[0][j] == 'O') dfs(board,n,m,0,j);
+        if(board[n - 1][j] == 'O') dfs(board,n,m,m - 1,j);
+    }
+    for(int i = 0;i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(board[i][j] == 'O') board[i][j] = 'X';
+            if(board[i][j] == '#') board[i][j] = 'O';
+        }
+    }
+}
 int main() {
     vector<vector<char>> board = {
         {'X', 'X', 'X', 'X'},
