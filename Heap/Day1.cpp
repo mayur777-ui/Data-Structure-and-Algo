a94150a920724f0857e71b9c1be40b4dae5cb141
mayur777-ui <mayurlaxkar76@gmail.com>
2025-09-08 15:39:55 +0530

    Enhance heap implementation with detailed debug output and comments

--- Heap/Day1.cpp
+++ Heap/Day1.cpp
@@ -1,4 +1,4 @@
-// // Implementation of heap
+// // // Implementation of heap
 #include <iostream>
 using namespace std;
 
@@ -108,34 +108,27 @@ int main() {
 void heapify(int arr[], int i, int n){
     int largest = i;
     int left = 2 * i + 1;
+    cout << "left-index: " <<  "2 *" << i << "+ 1 " << " = " << left << "\n";
     int right = 2 * i + 2;
+    cout << "right-index: " <<  "2 *" << i << "+ 2 " << " = " << right << "\n";
     if(left < n && arr[left] > arr[largest]){
+        cout << "left: " << arr[left] << "\n";
         largest = left;
     }
     if(right < n && arr[right] > arr[largest]){
+        cout << "right: " << arr[right] << "\n";
         largest = right;
     }
 
     if(largest != i){
+        cout << "largest before swap: " << arr[largest] << "\n";
         swap(arr[i],arr[largest]);
+        cout << "after swap largest: " << arr[largest] << "\n";
         heapify(arr, largest, n);
     }
 }
-void builtheap(int arr[],int n){
-    for(int i = n/2 - 1; i >= 0; i--){
-        heapify(arr,i,n);
-    }
-}
 
-// heap sort
 
-void heapsort(int arr[],int n){
-    builtheap(arr,n);
-    for(int i = n - 1; i >= 0; i--){
-        swap(arr[0],arr[i]);
-        heapify(arr,0,i);
-    }
-}
 
 void print(int arr[], int n){
     for(int i = 0; i < n; i++){
@@ -143,9 +136,39 @@ void print(int arr[], int n){
     }
 }
 
+void builtheap(int arr[],int n){
+    int round = 1;
+    for(int i = n/2 - 1; i >= 0; i--){
+        cout << "round: " << round << "\n";
+        cout << "start with: " << arr[i] << " at index: " << i << "\n";
+        heapify(arr,i,n);
+        print(arr,n);
+        cout << "\n";
+        round++;
+    }
+}
+
+
+// heap sort
+
+void heapsort(int arr[],int n){
+    // builtheap(arr,n);
+    int round = 1;
+    for(int i = n - 1; i >= 0; i--){
+        cout << "\nround: " << round << "\n";
+        cout << arr[0] << " swap with " << arr[i] << "\n";
+        swap(arr[0],arr[i]);
+        cout << "after swap" << arr[0] << " " << arr[i] << " now start heapify where we pass arr 0 as index and " << i << "as lenght of arr" << "\n"; 
+        heapify(arr,0,i);
+        print(arr, n);
+        round++;
+    }
+}
+
 int main(){
     int arr[] = {10,3 ,8 ,9 ,5, 13 ,18 , 14 , 11 ,70};
-    builtheap(arr,10);
+    builtheap(arr,10);  
+    cout << "let's start with heap sort: \n";
     heapsort(arr,10);
     print(arr,10);
 }
